b8e6042b75a3ba41e3517c6f4f1d03f6ad379f5e
Freedom-ToCode <26cmccutcheon@slvusd.org>
2025-09-18 16:00:16 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 const searchButton = document.getElementById('searchButton');
 const searchBar = document.getElementById('searchBar');
 const iFrame = document.getElementById('iFrame');
+const title = document.getElementById('title');
 
 myButton.addEventListener('click', function() {
   iFrame.src = searchBar.value;
+  title.value = "Website Viewing"
 });
